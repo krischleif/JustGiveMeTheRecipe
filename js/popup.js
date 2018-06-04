12efb17dca3d5c4e9b563f833886a6bc49248002
Kristin Schleif <krischleif@gmail.com>
2018-06-04 14:20:55 -0500

    reorganize div classes, add isOnScreen check to exclude good sites

--- js/popup.js
+++ js/popup.js
@@ -1,6 +1,4 @@
 var classNames = [
-	'div[itemtype="http://schema.org/Recipe"]',
-	'div[itemtype="https://schema.org/Recipe"]',
     '.recipe',
 	'.innerrecipe',
 	'.easyrecipe',
@@ -13,13 +11,15 @@ var classNames = [
 	'.recipe-callout',
 	'.tasty-recipes',
     '.food-card',
-    '.recipebody'
+    '.recipebody',
+	'div[itemtype="http://schema.org/Recipe"]',
+	'div[itemtype="https://schema.org/Recipe"]',
 ];
 
 function show(){
     classNames.every(function(e){
 		let recipe = $(e);
-		if( recipe && recipe.length === 1 ){
+		if( recipe && recipe.length === 1 && !isOnScreen(recipe)){
 			recipe.addClass('popupRecipe_ext');
 			recipe.append('<button class="closeBtn_ext">x</button>');
 			return false;
@@ -33,6 +33,25 @@ function hide()
 	$('.popupRecipe_ext').removeClass('popupRecipe_ext');
 	$('.closeBtn_ext').hide();
 }
+
+function isOnScreen(el){
+
+    var win = $(window);
+
+    var viewport = {
+        top : win.scrollTop(),
+        left : win.scrollLeft()
+    };
+    viewport.right = viewport.left + win.width();
+    viewport.bottom = viewport.top + win.height();
+
+    var bounds = el.offset();
+    bounds.right = bounds.left + el.outerWidth();
+    bounds.bottom = bounds.top + el.outerHeight();
+
+    return (!(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom));
+
+};
 
 chrome.storage.sync.get(document.location.hostname, function(items) {
 	show();
